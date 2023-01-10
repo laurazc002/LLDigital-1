2e72ff119939050a45ad515c05c0ca226263b58d
Luis Ignacio Villa Pino <livilla@tcc.com.co>
2023-01-10 15:58:11 -0500

    navegador pendiente

--- controllers/navegador.js
+++ controllers/navegador.js
@@ -3,8 +3,9 @@ let despliegue= document.getElementById('despliegue');
 let despliegue__opciones_capa1=document.getElementById('despliegue__opciones-capa1');
 
 let despliegue__opciones_capa2=document.getElementById('despliegue__opciones-capa2');
+let ampliarServicio=document.getElementById('ampliarServicio');
 
-let cerrar=document.getElementById('cerrar');
+ let cerrar=document.getElementById('cerrar');
 
 
 
@@ -16,20 +17,29 @@ despliegue.addEventListener('click',()=>{
 })
 
 
-despliegue__opciones_capa1.addEventListener('click',()=>{
-    
-    despliegue__opciones_capa2.classList.add('despliegue__opciones-capa2__activado');
-    
-    despliegue__opciones_capa2.classList.remove('despliegue__opciones-capa2');
-})
 
-cerrar.addEventListener('click',()=>{
-    despliegue__opciones_capa2.classList.add('despliegue__opciones-capa2');
+ cerrar.addEventListener('click',()=>{
+   despliegue__opciones_capa1.classList.remove('despliegue__opciones-capa1__activado');
     
-    despliegue__opciones_capa2.classList.remove('despliegue__opciones-capa2__activado');
-    
-    despliegue__opciones_capa1.classList.remove('despliegue__opciones-capa1__activado');
-
     despliegue__opciones_capa1.classList.add('despliegue__opciones-capa1');
+    
+    })
+    ampliarServicio.addEventListener('mouseover',function(){
+        despliegue__opciones_capa2.classList.remove('despliegue__opciones-capa2');
+        
+        despliegue__opciones_capa2.classList.add('despliegue__opciones-capa2__activo');
+    });
+    
+    
+    despliegue__opciones_capa2.addEventListener('mouseleave',()=>{
+        
+        despliegue__opciones_capa2.classList.remove('despliegue__opciones-capa2__activo');
+        despliegue__opciones_capa2.classList.add('despliegue__opciones-capa2');    
+    });
+    
+    
+    despliegue__opciones_capa1.addEventListener('mouseover',()=>{
+        despliegue__opciones_capa1.classList.add('despliegue__opciones-capa1'); 
+        despliegue__opciones_capa1.classList.remove('despliegue__opciones-capa1__activo');
+            });
 
-})
