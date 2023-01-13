7835c93e9a4919f90ec910484297cb4548cefd4e
Laura Maria Zuluaga Castañeda <lmzuluaga@tcc.com.co>
2023-01-13 16:01:31 -0500

    Alerta

---
+++ controllers/portafolio2.js
@@ -0,0 +1,26 @@
+Swal.fire({
+    // background: "rgba(18, 54, 131, 0.66)",
+    html: '<main class="bg-body rounded segundo"> <section> <section class="row "> <section class="col-3 text-center"> <img src="../img/portafolio2 (1).png" alt="" height="200px" width="300px" id="fotoPortafolio1"><section/> <section class="col-3 align-self-center text-center"><p><p/>  <section/> <section/> <main/>',
+    // icon:"question",
+    // confirmButtonText:"X",
+    width: "900px",
+    height:"600px",
+    padding: 0,
+    margin: 0,
+    grow: "row-2",
+    backdrop: true,
+    position: "center",
+    allowEscapeOutsideClick: true,
+    allowEscapeKey: false,
+    allowEnterkey: false,
+    stopkeydownPropagation:"false",
+    cancelButtonAriaLabel:true,
+    showConfirmButton:false,
+	showCancelButton:false,
+    showCloseButton: true,
+    // buttonsStyling:"#000"
+
+
+
+
+})
